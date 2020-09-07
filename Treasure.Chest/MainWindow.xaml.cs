1bf4b0c5731a7725d02b9fd9e0bdb26ed4b2ec6c
Ac.skir@outlook.com <Ac.skir@outlook.com>
2020-09-07 12:35:48 +0200

    Created method for Generate random numbers.

--- Treasure.Chest/MainWindow.xaml.cs
+++ Treasure.Chest/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Treasure.Chest.Models;
+
 
 namespace Treasure.Chest
 {
@@ -24,5 +26,6 @@ namespace Treasure.Chest
         {
             InitializeComponent();
         }
+
     }
 }
--- Treasure.Chest/Models/RandomNumbers.cs
+++ Treasure.Chest/Models/RandomNumbers.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace Treasure.Chest.Models
 {
-    class RandomNumbers
+   public abstract class RandomNumbers
     {
+        public static int[] GenerateRandomNumbers()
+        {
+            int[] numbers = new int[4];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i]= random.Next(0,10);
+            }
+            return numbers;
+        }
     }
 }
