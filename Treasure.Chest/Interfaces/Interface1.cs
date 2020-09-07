e5340974f106c9a1f7f06a35c58330775fed8ece
Pilvi <pilviborjesson@gmail.com>
2020-09-07 11:43:45 +0200

    Add project files.

---
+++ Treasure.Chest/Interfaces/Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Treasure.Chest.Interfaces
+{
+    public interface IPlayer
+    {
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public int Score { get; set; }
+    }
+}
---
+++ Treasure.Chest/Models/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Treasure.Chest.Models
+{
+    class Player
+    {
+    }
+}
