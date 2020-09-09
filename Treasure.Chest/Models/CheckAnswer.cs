d37b6a6663314ff9ada8d4dab6a79d78cd38d4f2
Micaela Sternås <micaela.sternas@outlook.com>
2020-09-09 16:49:15 +0200

    Get PlayerGuess and get correctanswer to GameViewModel

--- Treasure.Chest/Models/CheckAnswer.cs
+++ Treasure.Chest/Models/CheckAnswer.cs
@@ -1,18 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Treasure.Chest.ViewModels;
 
 namespace Treasure.Chest.Models
 {
-    class CheckAnswer
+    abstract class CheckAnswer
     {
-        public bool CorrectValueAndPosition(int[] guess)
+        //metoden för att kontrollera om gissningen stämmer överens med rätt svar
+
+        //RandomNumbers randomNumbers = new RandomNumbers();
+        //GameViewModel playerAnswer = new GameViewModel();
+
+        
+        public static bool CorrectValueAndPosition(int[] playerGuess, int [] Numbers)
+
         {
+            for (int i = 0; i < playerGuess.Length;)
+            {
+                if (playerGuess[i] == Numbers[i])
+                {
+                    return true; //Byt färg på bakgrunden
+                }
+
+            }
+
+            return false;
+        }
+        public static bool CorrectValueWrongPosition(int[] playerGuess, int[] Numbers)
+        {
+            if (playerGuess[0] != Numbers[0])
+            {
+
+            }
+
             return true;
         }
-        public bool CorrectValueWrongPosition(int[] guess)
-        {
-            return true;
-        }
+
+        
+
     }
-}
+
+
+
+    }
+
