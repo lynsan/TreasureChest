1d1b1b057392a7bd9d99e4f26303f6d912937758
Lyn Santiago Enriquez <lysa1900@student.miun.se>
2020-09-16 10:21:27 +0200

    listing guesses and changing background colors

--- Treasure.Chest/Models/CheckAnswer.cs
+++ Treasure.Chest/Models/CheckAnswer.cs
@@ -10,27 +10,27 @@ namespace Treasure.Chest.Models
 
         public static int[] CheckValueAndPosition(int[] playerGuess, int[] Numbers)
         {
-            int[] checkValueAndPosition = new int[4];
+            int[] playerInput = new int[4];
             for (int i = 0; i < playerGuess.Length; i++)
             {
                 for (int j = 0; j < playerGuess.Length; j++)
                 {
                     if (playerGuess[i] == Numbers[i])
                     {
-                        checkValueAndPosition[i] = 1; //correct value and position
+                        playerInput[i] = 1; //correct value and position
                     }
                     else if (playerGuess[i] == Numbers[j])
                     {
-                        checkValueAndPosition[i] = 2; //correct value wrong position
+                        playerInput[i] = 2; //correct value wrong position
                         break;
                     }
                     else
                     {
-                        checkValueAndPosition[i] = 0; //totally wrong
+                        playerInput[i] = 0; //totally wrong
                     }
                 }
             }
-            return checkValueAndPosition;
+            return playerInput;
         }
 
 
--- Treasure.Chest/ViewModels/GameViewModel.cs
+++ Treasure.Chest/ViewModels/GameViewModel.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Globalization;
 using System.Text;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Xaml.Schema;
 using Treasure.Chest.Models;
 using Treasure.Chest.ViewModels.Base;
 using Treasure.Chest.Views;
@@ -23,21 +28,19 @@ namespace Treasure.Chest.ViewModels
         public int Num3 { get; set; }
         public int Num4 { get; set; }
        
-
         public int[] PlayerGuess { get; set; }
         public int[] CorrectAnswer { get; set; }
 
-        public string Position0_0 { get; set; }
-        public string Position0_1 { get; set; }
-        public string Position0_2 { get; set; }
-        public string Position0_3 { get; set; }
         #endregion
+
+        public ObservableCollection<Guess> Guesses { get; set; } = new ObservableCollection<Guess>();
+
 
         public GameViewModel()
         {
             GuessCommand = new RelayCommand(CompareAnswers);
             CorrectAnswer = StartViewModel.SendNumbers();
-       
+
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -56,21 +59,62 @@ namespace Treasure.Chest.ViewModels
         {
 
             GetPlayerGuess();
-            //MessageBox.Show(CorrectAnswer[0].ToString() + CorrectAnswer[1].ToString() + CorrectAnswer[2].ToString() + CorrectAnswer[3].ToString());
-            //MessageBox.Show(PlayerGuess[0].ToString() + PlayerGuess[1].ToString() + PlayerGuess[2].ToString() + PlayerGuess[3].ToString());
             int[] checkedAnswer = new int[4];
             checkedAnswer = CheckAnswer.CheckValueAndPosition(PlayerGuess,CorrectAnswer);
-            MessageBox.Show(checkedAnswer[0].ToString() + checkedAnswer[1].ToString() + checkedAnswer[2].ToString() + checkedAnswer[3].ToString());
-            Position0_0 = PlayerGuess[0].ToString(); 
-            Position0_1 = PlayerGuess[1].ToString();
-            Position0_2 = PlayerGuess[2].ToString();
-            Position0_3 = PlayerGuess[3].ToString();
+            MessageBox.Show(checkedAnswer[0].ToString() + checkedAnswer[1].ToString() + checkedAnswer[2].ToString() + checkedAnswer[3].ToString());    
+            Guesses.Add(new Guess()
+            {
+                FirstGuess = new SmallGuess { Number = Num1, CorrectType = CorrectType.CorrectNumber },
+                SecondGuess = new SmallGuess { Number = Num2, CorrectType = CorrectType.CorrectNumberAndPlace },
+                ThirdGuess = new SmallGuess { Number = Num3, CorrectType = CorrectType.Incorrect},
+                FourthGuess = new SmallGuess { Number = Num4, CorrectType = CorrectType.CorrectNumberAndPlace }
+            });
+
         }
-        
 
 
+    }
 
+    public class Guess
+    {
+       
+        public SmallGuess FirstGuess { get; set; }
+        public SmallGuess SecondGuess {get; set; }
+        public SmallGuess ThirdGuess {get; set; }
+        public SmallGuess FourthGuess {get; set; }
+    }
+    public class SmallGuess
+    {
+        public int Number { get; set; }
+        public CorrectType CorrectType { get; set; }
+    }
+    public enum CorrectType
+    {
+        CorrectNumberAndPlace,
+        CorrectNumber,
+        Incorrect
+    }
 
+    public class CorrectTypeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((CorrectType)value)
+            {
+                case CorrectType.CorrectNumberAndPlace:
+                    return new SolidColorBrush(Colors.Green);
+                case CorrectType.CorrectNumber:
+                    return new SolidColorBrush(Colors.Yellow);
+                case CorrectType.Incorrect:
+                    return new SolidColorBrush(Colors.Transparent);
 
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
