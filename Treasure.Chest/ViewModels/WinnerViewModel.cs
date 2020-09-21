3e0a742ff8b32ea6b92a065c16bb997db56a95df
Ac.skir@outlook.com <Ac.skir@outlook.com>
2020-09-21 10:35:00 +0200

    Creating a method for getting player score

--- Treasure.Chest/ViewModels/WinnerViewModel.cs
+++ Treasure.Chest/ViewModels/WinnerViewModel.cs
@@ -14,20 +14,23 @@ namespace Treasure.Chest.ViewModels
         Player player= new Player();
         public string MyName { get; set; }
         public ICommand SaveCommand { get; set; }
+      
 
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         public WinnerViewModel()
         {
-            SaveCommand = new RelayCommand(AddPlayerName);
+            SaveCommand = new RelayCommand(AddPlayer);
         }
 
 
-        public void AddPlayerName()
+        public void AddPlayer()
         {
             player.Name = MyName;
+            player.Score=GameViewModel.GetScore(); 
             PlayerRepository.AddPlayer(player);
+
 
         }
     }
