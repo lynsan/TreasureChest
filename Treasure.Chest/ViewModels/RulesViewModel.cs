8ca96817c1d9fec42f22502f21c7eebab77a0a38
Micaela Sternås <micaela.sternas@outlook.com>
2020-09-30 09:48:13 +0200

    Changed font in highscore and points and time in winner page. Adjustments to rules text

--- Treasure.Chest/ViewModels/RulesViewModel.cs
+++ Treasure.Chest/ViewModels/RulesViewModel.cs
@@ -10,10 +10,13 @@ namespace Treasure.Chest.ViewModels
     class RulesViewModel
     {
         public ICommand BackCommand { get; set; }
-        public string GameRules { get; set; } = "You are a code breaker, and your goal is to guess the secret code to open the treasure chest." +
-            " The code is a sequence of numbers between 0 and 9, and note that the same number can take place several times. " +
-            "In each round after you make a guess, you will get hints which will lead you closer to guessing the secret code. " +
-            "Repeat this until you figure out the secret code. The hints are either GREEN background or YELLOW background: " +
+        public string GameRules { get; set; } = "You are a code breaker!" +
+            "\nYour goal is to guess the secret code to open the treasure chest." +
+            "\n" +
+            " \nThe code is a sequence of numbers between 0 and 9, and note that the same number can take place several times. " +
+            "After you make a guess, you will get hints which will lead you closer to guessing the secret code. " +
+            //"Repeat this until you figure out the secret code. The hints are either GREEN background or YELLOW background: " +
+            "\n" +
             "\nGREEN background = your guess is correct and is in the correct position " +
             "\nYELLOW background = your guess is correct but is in the wrong position";
 
